1685df4c046819b5fe7d3adeac371bc3ea66c740
Jaya Lakshmi Tangirala <jt3951@hallam.shu.ac.uk>
2025-10-16 16:15:53 +0100

    Add project files.

---
+++ LinearProbingHashTable.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+    internal class LinearProbingHashTable
+    {
+        private int[] hashTable;
+        private int tableSize, hashValue, emptyValue = -1; // Use -1 to indicate empty slot
+
+        // Constructor
+        public LinearProbingHashTable(int tableSize,int hash)
+        {            
+            this.tableSize = tableSize;
+            hashValue = hash;
+            hashTable = new int[tableSize];
+            for (int i = 0; i < tableSize; i++)
+                hashTable[i] = emptyValue;                        
+        }
+        private int Hash(int key)
+        {            
+            return key % hashValue;
+        }
+        // Insert using linear probing
+        public void Insert(int key)
+        {
+            int index = Hash(key);
+            int startIndex = index;
+
+            while (hashTable[index] != emptyValue)
+            {
+                Console.WriteLine($"Collision at index {index} for key {key}, trying next index...");
+                index = (index + 1) % hashTable.Length;
+
+                // Table full check
+                if (index == startIndex)
+                {
+                    Console.WriteLine("Hash table is full, cannot insert key " + key);
+                    return;
+                }
+            }
+
+            hashTable[index] = key;
+            Console.WriteLine($"Inserted key {key} at index {index}");
+        }
+
+        // Search in hash table
+        public int Search(int key)
+        {
+            int index = Hash(key);
+            int startIndex = index;
+
+            while (hashTable[index] != emptyValue)
+            {
+                if (hashTable[index] == key)
+                    return index;
+
+                index = (index + 1) % tableSize;
+                if (index == startIndex)
+                    break; // Came back to start
+            }
+
+            return -1; // Not found
+        }
+
+        // Display hash table
+        public void Display()
+        {
+            Console.WriteLine("Hash Table:");
+            for (int i = 0; i < tableSize; i++)
+            {
+                if (hashTable[i] != emptyValue)
+                    Console.WriteLine($"Index {i}: {hashTable[i]}");
+                else
+                    Console.WriteLine($"Index {i}: Empty");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace HashTable
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            
+            int[] keysToInsert = { 18, 26, 34, 42, 51, 40, 67, 45 };            
+            LinearProbingHashTable table = new LinearProbingHashTable(8,7); // Hash table size is 8 and value to use in hash function is 7
+            Console.WriteLine("table length is " + keysToInsert.Length);
+
+            foreach (int key in keysToInsert)
+                table.Insert(key);
+
+            table.Display();
+
+            int searchKey = 42;
+            int foundIndex = table.Search(searchKey);
+            if (foundIndex != -1)
+                Console.WriteLine($"Key {searchKey} found at index {foundIndex}");
+            else
+                Console.WriteLine($"Key {searchKey} not found in the hash table");
+        }
+    }
+}
